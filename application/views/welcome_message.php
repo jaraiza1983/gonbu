0a6621da36a432ce66646641672423f7bfba1544
Jonathan <jaraiza1983@gmail.com>
2018-06-21 17:21:11 -0700

    Creating Branch and Upload

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter Testing Commit 1</title>
+	<title>Welcome to CodeIgniter Setup Authentication</title>
 
 	<style type="text/css">
 
